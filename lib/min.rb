ac75e38ecf35ff180dcb350e54aaedaa10842195
Romena Murgor <romenamurgor@gmail.com>
2020-03-16 05:50:34 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -1,1 +1,15 @@
-ues.flatten.min
+equire 'pry'
+
+#This is the groceries hash we'll be passing in to the get_the_min method
+# groceries = {
+#  dairy: ["milk", "yogurt", "cheese"],
+#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  meat: ["chicken", "steak", "salmon"],
+#  grains: ["rice", "pasta"]
+# }
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
